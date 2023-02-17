b3a7253a81c87a4531bad5c32ab2f8b7f8d37e87
Mert Dağıstan <48259052+mertdagistan@users.noreply.github.com>
2023-02-17 19:22:52 +0300

    first commit

---
+++ src/screens/races/racesScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const RacesScreen = () => {
+  return (
+    <View>
+      <Text>RacesScreen</Text>
+    </View>
+  );
+};
+
+export default RacesScreen;
